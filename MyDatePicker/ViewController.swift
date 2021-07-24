ea9eaa721e2015ce24c72d2e649b9f59cfbf99de
pow2267 <pow2267@gmail.com>
2021-07-24 22:30:06 +0900

    Make date picker and change label's value when date picker's value changes

--- MyDatePicker/ViewController.swift
+++ MyDatePicker/ViewController.swift
@@ -8,10 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateLabel: UILabel!
+    let dateFormatter: DateFormatter = {
+        let formatter: DateFormatter = DateFormatter()
+//        formatter.dateStyle = .medium
+//        formatter.timeStyle = .medium
+//        formatter.dateFormat = "yyyy/MM/dd hh:mm:ss"
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        return formatter
+    }()
+    
+    @IBAction func didDatePickerValueChanged(_ sender: UIDatePicker) {
+        let date: Date = sender.date // == self.datePicker.date (IBOutlet으로 객체를 연결해놔서 가능)
+        let dateString: String = self.dateFormatter.string(from: date)
+        
+        self.dateLabel.text = dateString
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // IBAction 대신 메서드를 이용해 타겟과 액션을 연결해주는 방법
+        self.datePicker.addTarget(self, action: #selector(self.didDatePickerValueChanged(_:)), for: UIControl.Event.valueChanged)
     }
 
 
